8b3b20a291910b821f1ff5f59c4ece29cc1b53a8
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2012-10-15 20:35:11 +0200

    extract function code to minted

--- minted_code_extractor.py
+++ minted_code_extractor.py
@@ -6,10 +6,10 @@ REGEX = py.std.re.compile(r'\\mintedfromtexofcode\{(.*)}{(.*)}')
 
 TEMPLATE = r"""
 \begin{minted}[linenos, firstnumber=%(num)d]{python}
-missing = %(name)s
 %(code)s
 \end{minted}
 """.strip()
+
 def codeparts(path):
     matches = []
     with path.open() as fp:
@@ -19,6 +19,25 @@ def codeparts(path):
                 matches.append(match.group(1,2))
     return matches
 
+def extractfunction(lines, name):
+    find = 'def '+name
+    for startidx, line in enumerate(lines):
+        if find in line:
+            break
+
+    endidx = startidx+1
+    while lines[endidx+1][0].isspace():
+        endidx += 1
+
+    while not lines[startidx-1][0].isspace():
+        startidx -= 1
+
+    return startidx, ''.join(lines[startidx:endidx])
+
+def extractfunction_from_path(code, itemname):
+    path, funcname = itemname.split(':')
+    lines = code.join(path).readlines()
+    return extractfunction(lines, funcname)
 
 def run(content, code, target):
     parts = []
@@ -28,11 +47,12 @@ def run(content, code, target):
     mapping = dict(parts)
     assert len(mapping) == len(parts)
     
-    for targetname in mapping:
+    for targetname, source in mapping.items():
+        num, text = extractfunction_from_path(code, source)
         code = TEMPLATE % {
             'name': targetname.replace('_', '\_'),
-            'num': 0,
-            'code': mapping[targetname],
+            'num': num,
+            'code': text,
         }
         target.join(targetname+'.tex').write(code)
 
