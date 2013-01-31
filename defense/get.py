3c06b66e7a3f3662224faeb845f7703ed053ae6d
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2013-01-31 16:17:09 +0100

    fix up defense stuff

--- defense/get.py
+++ defense/get.py
@@ -1,14 +1,18 @@
 import restkit
 
+base = 'https://raw.github.com/hakimel/reveal.js/master/'
 
 want = {
-    'vendor/reveal.js': 'https://raw.github.com/hakimel/reveal.js'
-                        '/blob/master/js/reveal.js',
+    'vendor/head.min.js': 'lib/js/head.min.js',
+    'vendor/reveal.js': 'js/reveal.js',
+    'vendor/reveal.css': 'css/reveal.css',
+    'vendor/theme-default.css': 'css/theme/default.css',
 }
 
 
 for target in want:
-    response = restkit.request(want[target])
+    print target
+    response = restkit.request(base + want[target])
     body = response.body_string()
     with open(target, 'w') as fp:
         fp.write(body)
